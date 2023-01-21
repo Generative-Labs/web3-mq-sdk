4642fda2ef57a191a0385dc209dea677d6971d3e
yj <1095778225@qq.com>
2023-01-21 11:50:03 +0800

    feat: add WalletSign in FollowOperate

--- src/utils.ts
+++ src/utils.ts
@@ -297,3 +297,33 @@ export const updateMessageLoadStatus = (msgList: Array<any>, msg: any, status = 
   }
   return msgList;
 };
+
+export function newDateFormat(time: number, format?: string) {
+  const t = new Date(time);
+  format = format || 'Y-m-d h:i:s';
+  let year = t.getFullYear();
+  let month = t.getMonth() + 1;
+  let day = t.getDate();
+  let hours = t.getHours();
+  let minutes = t.getMinutes();
+  let seconds = t.getSeconds();
+
+  const hash = {
+    y: year,
+    m: month,
+    d: day,
+    h: hours,
+    i: minutes,
+    s: seconds,
+  };
+  // 是否补 0
+  const isAddZero = (o: string) => {
+    return /M|D|H|I|S/.test(o);
+  };
+  return format.replace(/\w/g, (o) => {
+    // @ts-ignore
+    let rt = hash[o.toLocaleLowerCase()];
+    if (typeof rt === 'string') return rt;
+    return rt >= 10 || isAddZero(o) ? rt : `0${rt}`;
+  });
+}
